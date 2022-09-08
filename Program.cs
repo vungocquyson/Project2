2ffbde455084810c5003dd9df2851d65c93f16ab
sonvnq <80165594+vungocquyson@users.noreply.github.com>
2022-09-08 16:42:41 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeCheck
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string keyy;
+            do
+            {
+                Console.Write("Prime cheker \n input X:");
+                int x = Convert.ToInt32(Console.ReadLine());
+                int cout = 0;
+                for (int i = 2; i <= x; i++)
+                    if (x % i == 0)
+                        cout++;
+
+                if (cout == 1) Console.WriteLine("Prime");
+                else Console.WriteLine("Non Prime");
+                cout = 0;
+                Console.Write("Continue Press C Exit Press anyother Key:");
+                keyy = Console.ReadLine();
+            } while (keyy == "c");
+            Console.ReadKey();
+        }
+    }
+}
